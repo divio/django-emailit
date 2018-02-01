c82a31f83c7fcf368a58bbb9f844ad388694125a
Filipe Waitman <filwaitman@gmail.com>
2018-02-01 12:19:02 -0200

    Feature/code cleanup (#13)

--- emailit/utils.py
+++ emailit/utils.py
@@ -7,16 +7,20 @@ class force_language:
     def __init__(self, new_lang):
         self.new_lang = new_lang
         self.old_lang = translation.get_language()
+
     def __enter__(self):
-       translation.activate(self.new_lang)
+        translation.activate(self.new_lang)
+
     def __exit__(self, type, value, tb):
-       translation.activate(self.old_lang)
+        translation.activate(self.old_lang)
+
 
 def get_template_name(language, base, part, suffix):
     if language:
         return u'.'.join((base, part, language, suffix))
     else:
         return u'.'.join((base, part, suffix))
+
 
 def get_template_names(language, base, part, suffix):
     template_names = [
